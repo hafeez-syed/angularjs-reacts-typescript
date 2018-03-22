f18d05fa4cd77963eb49b1d655604f128508d928
Hafeez <feeziman007@hotmail.com>
2018-03-23 05:59:10 +1100

    added first hello world ts file and index file

---
+++ src/HelloWorld.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Hafeez Syed on 23/3/18.
+ */
+function SayHello() {
+    var x = "hello World!";
+    alert(x);
+}
---
+++ src/HelloWorld.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Hafeez Syed on 23/3/18.
+ */
+
+function SayHello() {
+    let x = "hello World!";
+    alert(x);
+}
