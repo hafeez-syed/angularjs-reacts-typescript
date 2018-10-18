63704e57d3bfe60c2360e4cca483011a4f4eb232
Hafeez Syed <hafeez.syed@nab.com.au>
2018-10-18 11:02:55 +1100

    movies and food apis added

--- app/common/services/apiService.ts
+++ app/common/services/apiService.ts
@@ -16,6 +16,23 @@ namespace app.common {
                 .then(response => response.json())
                 .then(data => data.items);
         }
+
+        public getFoodRecipes(item?: any): Promise<any> {
+            let queryItem: any = item ? `&q=${item}` : ``;
+            let url = `https://www.food2fork.com/api/search?key=028543b3f0556def4525c524a12b483f${queryItem}&page=2`;
+            return fetch(url)
+                .then(response => response.json())
+                .then(data => data.recipes);
+        }
+
+        public getMovies(titleOrId?: string): Promise<any> {
+            let regex = /^tt[0-9]+$/;
+            let searchQuery: any = regex.test(titleOrId) ? `i=${titleOrId}` : `s=${titleOrId}`;
+            let url = `http://www.omdbapi.com/?${searchQuery}&page=2&apikey=6f387ed6`;
+            return fetch(url)
+                .then(response => response.json())
+                .then(data => data.Search || data);
+        }
     }
 
     angular
