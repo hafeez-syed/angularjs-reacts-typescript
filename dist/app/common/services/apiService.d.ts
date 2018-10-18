584691d3279e9150a4c9efde10c8d33801785212
Hafeez <feeziman007@hotmail.com>
2018-10-18 19:35:04 +1100

    remove dist

--- dist/app/common/services/apiService.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace app.common {
-    interface IApiService {
-        getPopularLanguages(language: any): Promise<any>;
-    }
-    class ApiService implements IApiService {
-        constructor();
-        getPopularLanguages(language: any): Promise<any>;
-    }
-}
