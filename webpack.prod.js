e6ec685ea1e5a26a3cdc96300219bed4643e3cb9
Hafeez Syed <hafeez.syed@nab.com.au>
2018-10-26 02:17:20 +1100

    umd modules generated

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const { CheckerPlugin } = require('awesome-typescript-loader');
 
-module.exports = {
+/*module.exports = {
 	entry: './app/index.ts',
 	output: {
 		filename: 'nghost.js',
@@ -33,4 +33,52 @@ module.exports = {
     optimization: {
         minimize: true
     }
-};
+};*/
+module.exports = [
+    createModuleConfig('amd'),
+    createModuleConfig('commonjs2'),
+    createModuleConfig('umd'),
+    createModuleConfig('var'),
+    createModuleConfig('window'),
+    createModuleConfig('this'),
+];
+
+
+function createModuleConfig(target) {
+    return {
+        entry: './app/index.ts',
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+        },
+        module: {
+            rules: [
+                { test: /\.less$/, loader: ['style-loader', 'css-loader'], exclude: /node_modules/},
+                { test: /\.tsx?$/, loader: ['awesome-typescript-loader'], exclude: /node_modules/ },
+                { test: /\.jsx?$/, loader: ['babel-loader'], exclude: /node_modules/, enforce: 'pre' }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+            new CheckerPlugin(),
+            new webpack.ProvidePlugin({
+                'React': 'react',
+                'ReactDOM': 'react-dom',
+            })
+        ],
+        optimization: {
+            minimize: false
+        },
+        externals: {
+            'React': 'react',
+            'ReactDOM': 'react-dom'
+        },
+        output: {
+            path: path.resolve(__dirname, './dist/'),
+            filename: 'ng-host.' + target + '.js',
+            library: 'ngHost',
+            libraryTarget: target
+        },
+        mode: 'production',
+        name: target
+    };
+}
